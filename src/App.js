a0a2e24f00df328d4a316075f7343cd8c85e2339
Alexander Gavazov <alex@gavazov.net>
2023-07-27 20:14:54 +0300

    Trigger change test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='App'>
       <h1 className='App-title'>Hello World!</h1>
-      <h2 className='App-subTitle'>Simple React App (demo 1)</h2>
+      <h2 className='App-subTitle'>Simple React App (demo 2)</h2>
     </div>
   );
 }
