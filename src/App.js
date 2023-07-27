4c1dbc40c7f8ea24f8964456edbeb33f3a41fc78
Alexander Gavazov <alex@gavazov.net>
2023-07-27 12:17:13 +0300

    Hello World

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='App-title'>Hello World!</h1>
+      <h2 className='App-subTitle'>Simple React App</h2>
+    </div>
+  );
+}
+
+export default App;
